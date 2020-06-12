725b68dc25d2ff3da011cd945b3c8b10fbf5b274
Hayden-Neilson <haydenneilson5@gmail.com>
2020-06-11 18:57:15 -0600

    update on course work

--- notes-1.js
+++ notes-1.js
@@ -1,2 +1,4 @@
 // 6/3-8/2020
 // I have learned how to create a user and a group with permissions along with learning how to create a EC2 Instance
+
+// well i learned how to use putty and how to open and use a instance of ec2!
