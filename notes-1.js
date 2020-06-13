4e7d56ed07e62b49d15282aa0a1278f1bef221f9
Hayden-Neilson <haydenneilson5@gmail.com>
2020-06-12 21:34:46 -0600

    securtiy groups

--- notes-1.js
+++ notes-1.js
@@ -1,4 +1,6 @@
 // 6/3-8/2020
 // I have learned how to create a user and a group with permissions along with learning how to create a EC2 Instance
 
-// well i learned how to use putty and how to open and use a instance of ec2!
+//  6/11/20 well i learned how to use putty and how to open and use a instance of ec2!
+
+//  6/12/20 I learned about security groups and how they work with incoming and outgoing info from and too the instance
