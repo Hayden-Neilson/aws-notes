5e248c03f468732cb6c38ba46bcc24738873d456
Hayden-Neilson <haydenneilson5@gmail.com>
2020-06-13 13:16:52 -0600

    ips and second instance

--- notes-1.js
+++ notes-1.js
@@ -4,3 +4,5 @@
 //  6/11/20 well i learned how to use putty and how to open and use a instance of ec2!
 
 //  6/12/20 I learned about security groups and how they work with incoming and outgoing info from and too the instance
+
+//  6/13/20 I learned about ips and how to lauch a instance with certain data like a webpage with content along with how to use apache and started another instance
